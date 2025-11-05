b5ebd1b12735fd1f724407b965d9e2d500fb8845
sijinhui <62741773+sijinhui@users.noreply.github.com>
2025-11-05 14:15:31 +0800

    尝试添加一份日志到容器控制台

--- utils/logger.go
+++ utils/logger.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"io" // 导入 io 包
 	"log/slog"
 	"os"
 )
@@ -13,6 +14,9 @@ func init() {
 		panic(err)
 	}
 
+	// 使用 io.MultiWriter 将文件和标准输出 (os.Stdout) 合并
+	multiWriter := io.MultiWriter(logFile, os.Stdout)
+
 	// 创建一个替换时间的函数
 	customTimeFormat := "2006-01-02 15:04:05"
 	replaceTime := func(groups []string, a slog.Attr) slog.Attr {
@@ -23,9 +27,11 @@ func init() {
 		return a
 	}
 
-	Logger = slog.New(slog.NewJSONHandler(logFile, &slog.HandlerOptions{
+	Logger = slog.New(slog.NewJSONHandler(multiWriter, &slog.HandlerOptions{ // 将 multiWriter 传入
 		AddSource:   true,           // 记录错误位置
 		Level:       slog.LevelInfo, // 设置日志级别
 		ReplaceAttr: replaceTime,
 	}))
+
+	Logger.Info("Logger initialized. Outputting to both file and console.")
 }
